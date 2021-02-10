968a874d8d577d02aa8be92dca114dab34a5b3a4
root <root@debianClone.debianClone>
2021-02-10 16:52:05 +0200

    front page styling

--- front-page.php
+++ front-page.php
@@ -6,11 +6,11 @@ get_header();
 		<article class="content px-3 py-5 p-md-5 konsteus-article" >
 
 			<div class="konsteus-article-horizontal-strip-1" >
-				<div class="konsteus-article-horizontal-strip-container-1" >
+				<div class="konsteus-article-horizontal-strip-container-1 d-lg-flex" >
 
-				    <img src="/wp-content/themes/konsteus/assets/images/wrench.svg" class="konsteus-article-image" alt="Repair" width="250" height="250">
+				    <img src="/wp-content/themes/konsteus/assets/images/wrench.svg" class="konsteus-article-image img-fluid" alt="Repair" width="250" height="250">
 					<div class="konsteus-article-horizontal-strip-container-text-1" >
-						<h1>Tarbeelektroonika remont</h1>
+						<h1><span class="text-break">Tarbe</span>elektroonika remont</h1>
 						<p>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
 						 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -24,11 +24,11 @@ get_header();
 				</div>
 			</div>
 			<div class="konsteus-article-horizontal-strip-2" >
-				<div class="konsteus-article-horizontal-strip-container-2" >
+				<div class="konsteus-article-horizontal-strip-container-2 d-lg-flex" >
 
-				    <img src="/wp-content/themes/konsteus/assets/images/aircan.svg" class="konsteus-article-image" alt="Repair" width="250" height="250">
+				    <img src="/wp-content/themes/konsteus/assets/images/aircan.svg" class="konsteus-article-image img-fluid" alt="Repair" width="250" height="250">
 					<div class="konsteus-article-horizontal-strip-container-text-2" >
-						<h1>Tarbeelektroonika Hooldus</h1>
+						<h1><span class="text-break">Tarbe</span>elektroonika Hooldus</h1>
 						<p>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
 						 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -41,24 +41,7 @@ get_header();
 					</div>
 				</div>
 			</div>
-			<div class="konsteus-article-horizontal-strip-2" >
-				<div class="konsteus-article-horizontal-strip-container-2" >
-
-				    <img src="/wp-content/themes/konsteus/assets/images/aircan.svg" alt="Repair" width="250" height="250">
-					<div class="konsteus-article-horizontal-strip-container-text-2" >
-						<h1>Tarbeelektroonika Hooldus</h1>
-						<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-						 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-						  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-						  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-						  in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-						  Excepteur sint occaecat cupidatat non proident, 
-						  sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-					</div>
-				</div>
-			</div>
+			
 
 
 		 <?php 
